41747d5d95637f311af9defac0b0a9b341147991
Vipul Prajapati <vipulprajapati0070@gmail.com>
2025-01-15 15:09:50 +0000

    dom selection done

---
+++ src/main.js
@@ -0,0 +1,8 @@
+//Initial References
+const moves = document.getElementById("moves");
+const container = document.querySelector(".container");
+const startButton = document.getElementById("start-button");
+const coverScreen = document.querySelector(".cover-screen");
+const result = document.getElementById("result");
+
+
